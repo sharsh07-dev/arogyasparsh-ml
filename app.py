bdcf11f28a14b57233bc645e3b9eb6c16d2dc824
harsh shinde <shindeharsh.dev@gmail.com>
2025-12-09 14:17:56 +0530

    Added smart stock deduction on approval

--- app.py
+++ app.py
@@ -352,4 +352,4 @@ def phc_assistant():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5002))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port) #commit
